d8d1db90fd224cc1d07a5d30ab5e6eedac853a4a
brad@smithwebtek.com <brad@smithwebtek.com>
2021-06-08 08:08:03 -0400

    Initial commit

---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './ExpenseItem'
+
+
+function Expenses() {
+
+  return (
+    <div>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
+    </div>
+  )
+
+}
+
+export default Expenses
