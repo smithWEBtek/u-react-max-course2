1e2ddf30a89ecebf91e1a041c65e889f0134ca4d
brad@smithwebtek.com <brad@smithwebtek.com>
2021-07-13 12:46:51 -0400

    Solved

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+
   const renderedExpenses = props.items.map((expense, i) => (
     <ExpenseItem
       key={i}
@@ -12,10 +17,20 @@ const Expenses = (props) => {
       amount={expense.amount} />
   ))
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+    console.log('[Expenses.js] selectedYear: ', selectedYear);
+  }
+
   return (
+    <div>
     <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={filterChangeHandler}
+          selected={filteredYear} />
       {renderedExpenses}
     </Card>
+    </div>
   )
 }
 
