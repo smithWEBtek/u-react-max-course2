4da6f79ec28644da2b8bb9f0f545ab3f7151a32e
brad@smithwebtek.com <brad@smithwebtek.com>
2021-12-28 12:32:08 -0500

    Finish header, with styled dummy cart

--- 11-food-order-app/FoodOrderApp/src/components/Layout/Header.js
+++ 11-food-order-app/FoodOrderApp/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import mealsImg from '../../assets/meals.jpeg';
+import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
 
 const Header = props => {
   return <Fragment>
     <header className={classes.header}>
       <h1>ReactMeals</h1>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
       <img src={mealsImg} alt="meals-img" />
