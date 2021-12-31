7aa4e920e1daf3d13591234573db5849b509f4d9
brad@smithwebtek.com <brad@smithwebtek.com>
2021-12-31 12:35:15 -0500

    Thru 141, cart modal working

--- 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
+++ 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
@@ -1,5 +1,4 @@
 import Modal from '../UI/Modal/Modal';
-
 import classes from './Cart.module.css';
 
 const Cart = props => {
@@ -15,7 +14,7 @@ const Cart = props => {
   })
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       <div>
         <ul className={classes['cart-items']}>
           {cartItems}
@@ -26,7 +25,7 @@ const Cart = props => {
         <span>$35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal >
