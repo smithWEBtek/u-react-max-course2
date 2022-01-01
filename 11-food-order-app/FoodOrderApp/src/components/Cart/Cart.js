2499235f63a442411daa0b178c05513c7a970c49
brad@smithwebtek.com <brad@smithwebtek.com>
2022-01-01 08:51:03 -0500

    Start cart reducer

--- 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
+++ 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
@@ -2,22 +2,22 @@ import Modal from '../UI/Modal/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
-  const cartData = [
-    { id: 'c1', name: 'Sushi Treat', amount: 1, price: 12.99 },
-    { id: 'c2', name: 'Burger Bomb', amount: 1, price: 15.99 },
-    { id: 'c3', name: 'Caeser Salad', amount: 1, price: 5.99 },
-    { id: 'c4', name: 'Mock Turtle Soup', amount: 1, price: 4.99 }
-  ]
+  // const cartData = [
+  //   { id: 'c1', name: 'Sushi Sweet', amount: 1, price: 12.99 },
+  //   { id: 'c2', name: 'Burgee Bomb', amount: 1, price: 15.99 },
+  //   { id: 'c3', name: 'Cobb Salad', amount: 1, price: 5.99 },
+  //   { id: 'c4', name: 'Mock Muhi Soup', amount: 1, price: 4.99 }
+  // ]
 
-  const cartItems = cartData.map(item => {
-    return <li>{item.name}</li>
-  })
+  // const cartItems = cartData.map((item, index) => {
+  //   return <li key={`${item.name}_${item.id}_` + index}>{item.name}</li>
+  // })
 
   return (
     <Modal onClose={props.onClose}>
       <div>
         <ul className={classes['cart-items']}>
-          {cartItems}
+          {/* {cartItems} */}
         </ul>
       </div>
       <div className={classes.total}>
