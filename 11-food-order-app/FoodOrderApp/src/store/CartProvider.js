8bf5832bb98f5fb22061090ab502138bcbff4ef6
brad@smithwebtek.com <brad@smithwebtek.com>
2022-01-05 18:46:04 -0500

    Update cart provider

--- 11-food-order-app/FoodOrderApp/src/store/CartProvider.js
+++ 11-food-order-app/FoodOrderApp/src/store/CartProvider.js
@@ -8,7 +8,6 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
     const existingCartItem = state.items[existingCartItemIndex];
