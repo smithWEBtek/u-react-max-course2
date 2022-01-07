e5da80da1b26f684b882bf442691e1b7641f3b35
brad@smithwebtek.com <brad@smithwebtek.com>
2022-01-06 21:33:04 -0500

    Cart w/issues

--- 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
+++ 11-food-order-app/FoodOrderApp/src/components/Cart/Cart.js
@@ -10,8 +10,13 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => { };
-  const cartItemAddHandler = (cartItemRemoveHandler) => { };
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const cartItems = cartCtx.items.map((item, index) => {
     return <CartItem
--- 11-food-order-app/FoodOrderApp/src/store/cart-context.js
+++ 11-food-order-app/FoodOrderApp/src/store/cart-context.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CartContext = React.createContext({
   items: [],
-  totalAmount: 12.30,
+  totalAmount: 0,
   addItem: (item) => { },
   removeItem: (id) => { }
 })
