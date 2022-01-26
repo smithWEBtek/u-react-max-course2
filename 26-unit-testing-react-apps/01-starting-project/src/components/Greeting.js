2102f61d12fbb89374657f9808fbb0d436fe5f5f
brad@smithwebtek.com <brad@smithwebtek.com>
2022-01-26 13:57:29 -0500

    Tests with fireEvent

--- 26-unit-testing-react-apps/01-starting-project/src/components/Greeting.js
+++ 26-unit-testing-react-apps/01-starting-project/src/components/Greeting.js
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+
+
 const Greeting = () => {
-  return <div><h2>Hello World!</h2>
-    <p>It's good to see you!</p></div>
+  const [changedText, setChangedText] = useState(false);
+
+  const changedTextHandler = () => {
+    setChangedText(!changedText);
+  };
+
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      {!changedText && <p>It's good to see you!</p>}
+      {/* <p>It's good to see you!</p> */}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changedTextHandler}>Change the Text!</button>
+    </div>
+  )
 }
 
 export default Greeting;
--- 26-unit-testing-react-apps/01-starting-project/src/components/Greeting.test.js
+++ 26-unit-testing-react-apps/01-starting-project/src/components/Greeting.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Greeting from './Greeting';
 
 describe('description of category: Greeting Component', () => {
@@ -14,9 +14,41 @@ describe('description of category: Greeting Component', () => {
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test('paragraph greeting text', () => {
+  test('renders correct paragraph text before button click', () => {
     render(<Greeting />)
-    const greetingElement = screen.getByText(/It's good to see you!/)
+    const greetingElement = screen.getByText("It's good to see you!", { exact: false })
     expect(greetingElement).toBeInTheDocument();
+  });
+
+  test('renders correct paragraph text after button click', () => {
+    // Arrange
+    render(<Greeting />)
+    // Act: click button
+    const buttonElement = screen.getByRole('button');
+    fireEvent.click(buttonElement)
+    // Assert
+    const outputElement = screen.getByText('Changed!', { exact: false })
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  // test('does not render default paragraph after button click', () => {
+  //   render(<Greeting />)
+  //   const buttonElement = screen.getByRole('button');
+  //   const greetingElement = screen.getByText("It's good to see you!", { exact: false })
+  //   fireEvent.click(buttonElement)
+
+  //   expect(greetingElement).not.toBeInTheDocument()
+  // });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    // Arrange    
+    render(<Greeting />)
+    // Act    
+    const buttonElement = screen.getByRole('button');
+    fireEvent.click(buttonElement)
+    const outputElement = screen.queryByText('good to see you', { exact: false })
+    // Assert
+    // expect(outputElement).not.toBeInTheDocument();
+    expect(outputElement).toBeNull();
   });
 });
